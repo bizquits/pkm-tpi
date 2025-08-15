f187e3add7c410cd4645ab5bfab30e8dbc315c8a
faridpahlevi <faridpahlevi@student.uir.ac.id>
2025-08-15 20:47:07 +0700

    Update form & js

--- src/js/form.js
+++ src/js/form.js
@@ -16,52 +16,78 @@ form.addEventListener("submit", (e) => {
   let svg = "";
   let button = "";
 
-  const headache = document.querySelector(
-    'input[name="headache"]:checked'
+  const batuk_2minggu = document.querySelector(
+    'input[name="batuk_2minggu"]:checked'
   )?.value;
-  const nausea = document.querySelector('input[name="nausea"]:checked')?.value;
-  const diarrhea = document.querySelector(
-    'input[name="diarrhea"]:checked'
+  const demam_tak_jelas = document.querySelector(
+    'input[name="demam_tak_jelas"]:checked'
   )?.value;
-  const appetite = document.querySelector(
-    'input[name="appetite"]:checked'
+  const keringat_malam = document.querySelector(
+    'input[name="keringat_malam"]:checked'
   )?.value;
-  const sleep = document.querySelector('input[name="sleep"]:checked')?.value;
+  const berat_turun = document.querySelector(
+    'input[name="berat_turun"]:checked'
+  )?.value;
+  const lelah_lesu = document.querySelector(
+    'input[name="lelah_lesu"]:checked'
+  )?.value;
+  const nyeri_dada = document.querySelector(
+    'input[name="nyeri_dada"]:checked'
+  )?.value;
+  const dahak_darah = document.querySelector(
+    'input[name="dahak_darah"]:checked'
+  )?.value;
+  const kontak_keluarga = document.querySelector(
+    'input[name="kontak_keluarga"]:checked'
+  )?.value;
+  const kontak_lain = document.querySelector(
+    'input[name="kontak_lain"]:checked'
+  )?.value;
 
-  if (headache === "sedang" || headache === "parah") score++;
-  if (nausea === "sering") score++;
-  if (diarrhea === "berulang") score++;
-  if (appetite === "hilang sama sekali") score++;
-  if (sleep === "sulit tidur atau tidak tidur") score++;
+  if (batuk_2minggu === "iya") score++;
+  if (demam_tak_jelas === "iya") score++;
+  if (keringat_malam === "iya") score++;
+  if (berat_turun === "iya") score++;
+  if (lelah_lesu === "iya") score++;
+  if (nyeri_dada === "iya") score++;
+  if (dahak_darah === "iya") score++;
+  if (kontak_keluarga === "iya") score++;
+  if (kontak_lain === "iya") score++;
 
-  if (score >= 4) {
-    message =
-      "Berdasarkan jawaban Anda, disarankan segera memeriksakan diri ke dokter.";
+  if (score >= 3) {
+    message = `
+    <div class="text-center">
+      <p class="font-bold">Anda memiliki gejala TBC</p> 
+      <p>Ayo kunjungi layanan kesehatan terdekat untuk mendapatkan 
+      <span class="font-bold">PENGOBATAN TBC GRATIS</span>. TBC bisa diobati sampai sembuh.</p>
+      </div>`;
     color = "bg-red-100";
     svg = `<svg viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="1.5" data-slot="icon" aria-hidden="true" class="size-6 text-red-600">
                               <path d="M12 9v3.75m-9.303 3.376c-.866 1.5.217 3.374 1.948 3.374h14.71c1.73 0 2.813-1.874 1.948-3.374L13.949 3.378c-.866-1.5-3.032-1.5-3.898 0L2.697 16.126ZM12 15.75h.007v.008H12v-.008Z" stroke-linecap="round" stroke-linejoin="round" />
                           </svg>`;
-    button = `<a command="close" commandfor="dialog" href="https://wa.me/6282285803030?text=Halo%2C%20saya%20ingin%20berkonsultasi%20karena%20mengalami%20beberapa%20gejala.
-            " target="_blank" class="inline-flex w-full justify-center rounded-md bg-red-600 px-3 py-2 text-sm font-semibold text-white shadow-xs hover:bg-red-500 sm:ml-3 sm:w-auto">Lanjutkan</a>
-                  </div>`;
-  } else if (score >= 2) {
-    message =
-      "Anda mengalami beberapa gejala. Istirahat dan pantau kondisi Anda.";
-    color = "bg-yellow-100";
-    svg = `<svg viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="1.5" data-slot="icon" aria-hidden="true" class="size-6 text-yellow-600">
-                              <path d="M12 9v3.75m-9.303 3.376c-.866 1.5.217 3.374 1.948 3.374h14.71c1.73 0 2.813-1.874 1.948-3.374L13.949 3.378c-.866-1.5-3.032-1.5-3.898 0L2.697 16.126ZM12 15.75h.007v.008H12v-.008Z" stroke-linecap="round" stroke-linejoin="round" />
-                          </svg>`;
-    button = `<button type="button" command="close" commandfor="dialog" class="inline-flex w-full justify-center rounded-md bg-yellow-600 px-3 py-2 text-sm font-semibold text-white shadow-xs hover:bg-yellow-500 sm:ml-3 sm:w-auto">Tutup</button>
-                  </div>`;
+    button = `<a command="close" commandfor="dialog" href="https://wa.me/6282285803030?text=Saya%20memiliki%20gejala%20TBC%20dan%20saya%20ingin%20mendapatkan%20pengobatan.
+            " target="_blank" class="inline-flex w-full justify-center rounded-md bg-red-600 px-3 py-2 text-sm font-semibold text-white shadow-xs hover:bg-red-500 sm:ml-3 sm:w-auto">Lindungi Keluarga Anda</a>
+              `;
   } else {
-    message =
-      "Kondisi Anda tampaknya ringan. Tetap jaga kesehatan dan perhatikan gejala lanjutan.";
+    message = `
+      <p><strong>Tetap jaga kesehatan dengan mencegah penularan TBC:</strong></p>
+      <div class="ml-5">
+      <ul class="list-decimal sm:text-left">
+        <li>Gunakan masker bila batuk</li>
+        <li>Cuci tangan teratur</li>
+        <li>Istirahat yang cukup</li>
+        <li>Konsumsi gizi seimbang</li>
+        <li>Hindari asap rokok</li>
+        <li>Terapkan etika batuk dan bersin yang benar</li>
+      </ul>
+      </div>
+    `;
     color = "bg-green-100";
     svg = `<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6 text-green-600">
                             <path stroke-linecap="round" stroke-linejoin="round" d="M9 12.75 11.25 15 15 9.75M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z" />
                         </svg>`;
-    button = `<button type="button" command="close" commandfor="dialog" class="inline-flex w-full justify-center rounded-md bg-green-600 px-3 py-2 text-sm font-semibold text-white shadow-xs hover:bg-green-500 sm:ml-3 sm:w-auto">Tutup</button>
-                  </div>`;
+    button = `<a command="close" commandfor="dialog" href="https://dinkes.kepriprov.go.id/blog/viewberita/waspada-tuberkulosis-kenali-gejalanya-cegah-penularannya-dan-tuntaskan-pengobatannya" target="_blank" class="inline-flex w-full justify-center rounded-md bg-green-600 px-3 py-2 text-sm font-semibold text-white shadow-xs hover:bg-green-500 sm:ml-3 sm:w-auto">Informasi Lebih Lanjut</a>
+              `;
   }
 
   e.preventDefault();
@@ -71,7 +97,7 @@ form.addEventListener("submit", (e) => {
     .then((response) => {
       btn.classList.toggle("hidden");
       btnLoad.classList.toggle("hidden");
-      modalErrContent.textContent = message;
+      modalErrContent.innerHTML = message;
       modalSvg.innerHTML = svg;
       modalSvg.classList.add(color);
       modalButton.innerHTML = button;
--- src/js/script.js
+++ src/js/script.js
@@ -7,6 +7,10 @@ document.addEventListener("DOMContentLoaded", function () {
   const phoneError = document.getElementById("phoneError");
   const phoneErrContent = document.getElementById("phoneErrContent");
 
+  const nikInput = document.getElementById("nik");
+  const nikError = document.getElementById("nikError");
+  const nikErrContent = document.getElementById("nikErrContent");
+
   nameInput.addEventListener("input", function () {
     const nameValue = nameInput.value.trim();
     const nameRegex = /^[A-Za-z\s]*$/;
@@ -27,12 +31,31 @@ document.addEventListener("DOMContentLoaded", function () {
     if (!phoneRegex.test(phoneValue)) {
       phoneErrContent.textContent = "Hanya boleh angka.";
       phoneError.classList.remove("hidden");
+    } else if (phoneValue.length < 10) {
+      phoneErrContent.textContent = "Masukkan nomor handphone yang sesuai";
+      phoneError.classList.remove("hidden");
     } else if (phoneValue.length > 13) {
       phoneErrContent.textContent = "Maksimal 13 digit.";
       phoneError.classList.remove("hidden");
     } else {
       phoneErrContent.textContent = "";
       phoneError.classList.add("hidden");
+    }
+  });
+
+  nikInput.addEventListener("input", function () {
+    const nikValue = nikInput.value.trim();
+    const nikRegex = /^[0-9]*$/;
+
+    if (!nikRegex.test(nikValue)) {
+      nikErrContent.textContent = "Hanya boleh angka.";
+      nikError.classList.remove("hidden");
+    } else if (nikValue.length != 16) {
+      nikErrContent.textContent = "NIK tidak sesuai";
+      nikError.classList.remove("hidden");
+    } else {
+      nikErrContent.textContent = "";
+      nikError.classList.add("hidden");
     }
   });
 });
