8a870f1bd786562e32692acb2d97dcc8dba9547c
faridpahlevi <faridpahlevi@student.uir.ac.id>
2025-08-16 21:00:18 +0700

    add jk & address form

--- src/js/form.js
+++ src/js/form.js
@@ -1,6 +1,6 @@
 const scriptURL =
   "https://script.google.com/macros/s/AKfycbzV7P_VNOYJ8yk5JqCuWm9cunYTiKFIqsPGRgI6kavjOwPl4rCQ_I_6492OGIW0y-_q/exec";
-const form = document.forms["SelfAssessment"];
+const form = document.forms["pkm-tpi"];
 const btn = document.querySelector(".btn");
 const btnLoad = document.querySelector(".btn-load");
 const alert = document.getElementById("modal");
@@ -65,7 +65,7 @@ form.addEventListener("submit", (e) => {
     svg = `<svg viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="1.5" data-slot="icon" aria-hidden="true" class="size-6 text-red-600">
                               <path d="M12 9v3.75m-9.303 3.376c-.866 1.5.217 3.374 1.948 3.374h14.71c1.73 0 2.813-1.874 1.948-3.374L13.949 3.378c-.866-1.5-3.032-1.5-3.898 0L2.697 16.126ZM12 15.75h.007v.008H12v-.008Z" stroke-linecap="round" stroke-linejoin="round" />
                           </svg>`;
-    button = `<a command="close" commandfor="dialog" href="https://wa.me/6282285803030?text=Saya%20memiliki%20gejala%20TBC%20dan%20saya%20ingin%20mendapatkan%20pengobatan.
+    button = `<a command="close" commandfor="dialog" href="https://wa.me/628127079122?text=Saya%20memiliki%20gejala%20TBC%20dan%20saya%20ingin%20mendapatkan%20pengobatan.
             " target="_blank" class="inline-flex w-full justify-center rounded-md bg-red-600 px-3 py-2 text-sm font-semibold text-white shadow-xs hover:bg-red-500 sm:ml-3 sm:w-auto">Lindungi Keluarga Anda</a>
               `;
   } else {
